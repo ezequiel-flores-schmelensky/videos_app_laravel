cc7db28da1cedcf631efb98df151e6337b2a8fac
Ezequiel Flores <supereze.bxb@gamil.com>
2019-09-20 15:09:24 -0500

    [feat] videos list

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use App\Http\Requests;
 use Illuminate\Http\Request;
+use App\Video;
 
 class HomeController extends Controller
 {
@@ -24,6 +25,9 @@ class HomeController extends Controller
      */
     public function index()
     {
-        return view('home');
+        $videos = Video::orderBy('id', 'desc')->paginate(5);
+        return view('home', array(
+            'videos' => $videos
+        ));
     }
 }
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -3,15 +3,28 @@
 @section('content')
 <div class="container">
     <div class="row">
-        <div class="col-md-10 col-md-offset-1">
-            <div class="panel panel-default">
-                <div class="panel-heading">Dashboard</div>
-
-                <div class="panel-body">
-                    You are logged in!
+        <div class="container">
+            @if(session('message'))
+                <div class="alert alert-success">
+                    {{ session('message') }}
                 </div>
-            </div>
+            @endif
+
+            <ul id="videos-list">
+                @foreach($videos as $video)
+                    <li class="video-item col-md-4 pull-left">
+                        <!-- imagen del video -->
+                        <div class="data">
+                            <h4>{{$video->title}}</h4>
+                        </div>
+                        <!-- BOTONES DE ACCIÓN -->
+                    </li>
+                @endforeach
+            </ul>
         </div>
+
+        {{$videos->links()}}
+
     </div>
 </div>
 @endsection
