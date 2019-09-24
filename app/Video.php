2a0c81f1b15c0d55a603b9964b688eec22277437
Ezequiel Flores <supereze.bxb@gamil.com>
2019-09-24 11:31:27 -0500

    [feat] comments list

--- app/Video.php
+++ app/Video.php
@@ -10,7 +10,7 @@ class Video extends Model
 
     // Relación One To Many
     public function comments() {
-        return $this->hasMany('App\Comment');
+        return $this->hasMany('App\Comment')->orderBy('id', 'desc');
         //hasOne
     }
 
--- resources/views/video/comments.blade.php
+++ resources/views/video/comments.blade.php
@@ -8,6 +8,7 @@
     </div>
 @endif
 
+@if(Auth::check())
 <form action="{{url('/comment') }}" method="POST" class="col-md-4">
     {!! csrf_field() !!}
     <input type="hidden" name="video_id" value="{{ $video->id }}" required />
@@ -16,3 +17,25 @@
     </p>
     <input type="submit" value="Comentar" class="btn btn-success"/>
 </form>
+<div class="clearfix"></div>
+<hr/>
+@endif
+
+@if(isset($video->comments))
+    <div id="comments-list">
+        @foreach($video->comments as $comment)
+            <div class="comment-item col-md-12 pull-left">
+                <div class="panel panel-default comment-data">
+                    <div class="panel-heading">
+                        <div class="panel-title">
+                            <strong>{{ $comment->user->name.' '.$comment->user->surname}}</strong> {{ \FormatTime::LongTimeFilter($comment->created_at) }}
+                        </div>
+                    </div>
+                    <div class="panel-body">
+                        {{ $comment->body }}
+                    </div>
+                </div>
+            </div>
+        @endforeach
+    </div>
+@endif
